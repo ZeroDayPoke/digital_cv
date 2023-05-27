15937e0ef446d70ffc5ebd0f3289df1d4554f2d4
ZeroDayPoke <cstampok@gmail.com>
2023-05-27 16:00:57 -0500

    name mismatch

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,7 @@
 # app/routes/main.py
 from flask import Blueprint
 
-main_routes = Blueprint('main', __name__, url_prefix='')
+main_routes = Blueprint('main_routes', __name__, url_prefix='')
 
 @main_routes.route('/')
 def index():
