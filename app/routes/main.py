884f29efcadcb29778f798763e47e235fe5c39aa
ZeroDayPoke <cstampok@gmail.com>
2023-05-27 17:06:52 -0500

    janga template blocks live test and icon

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python3
+"""
+main.py - main routes for the Flask application
+"""
 # app/routes/main.py
-from flask import Blueprint
+
+from flask import Blueprint, render_template
 
 main_routes = Blueprint('main_routes', __name__, url_prefix='')
 
 @main_routes.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('index.html')
