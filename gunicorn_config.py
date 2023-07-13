1475806b1cdb26727435ad92ef808e07ea3ceb50
Chris Stamper <cstampok@gmail.com>
2023-07-12 20:20:35 -0500

    wsgi gunicorn

---
+++ gunicorn_config.py
@@ -0,0 +1,10 @@
+bind = '127.0.0.1:8000'
+workers = 4
+threads = 2
+worker_class = 'sync'
+preload_app = True
+max_requests = 1000
+max_requests_jitter = 50
+graceful_timeout = 30
+accesslog = '-'
+errorlog = '-'
---
+++ wsgi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+wsgi.py
+
+This is the entry point for the WSGI server.
+
+Located in /digital_cv/wsgi.py
+"""
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port='8000', threaded=True)
