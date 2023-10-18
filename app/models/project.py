6a7f9b6a7f102e649f8e2a7e4a9c0a07d2cdf7b9
Chris Stamper <cstampok@gmail.com>
2023-10-18 18:45:22 -0500

    no timestamp since base model

--- app/models/project.py
+++ app/models/project.py
@@ -23,6 +23,8 @@ class Project(BaseModel):
         The link to the project's repository.
     live_link : str
         The link to the live version of the project.
+    misc_link : str
+        The link to any other relevant information about the project.
     related_skills : list of Skill objects
         The list of skills related to the project.
     """
@@ -32,6 +34,7 @@ class Project(BaseModel):
     role = db.Column(db.String(120), nullable=True)
     repo_link = db.Column(db.String(500), nullable=True)
     live_link = db.Column(db.String(500), nullable=True)
+    misc_link = db.Column(db.String(500), nullable=True)
     related_skills = db.relationship('Skill', secondary=project_skills, back_populates='related_projects')
 
     def __repr__(self):
