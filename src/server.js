5c4c645fc5948a8af04d3e00b51d277d30d1379c
Chris Stamper <cstampok@gmail.com>
2023-11-11 12:35:18 -0600

    test

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.use(
 );
 
 // Define allowed origins
-const allowedOrigins = ["https://zerodaypoke.com", "https://www.zerodaypoke.com", "node-app:3000", "localhost:8000"];
+const allowedOrigins = ["https://zerodaypoke.com:8000", "https://zerodaypoke.com/send_verification_email", "node-app:3000", "localhost:8000"];
 
 // Set up CORS policy
 app.use(
