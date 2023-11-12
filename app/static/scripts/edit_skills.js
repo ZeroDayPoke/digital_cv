e237f46ed25db9c629738bdf470bcb04ac3d5fe3
Chris Stamper <cstampok@gmail.com>
2023-11-12 16:59:47 -0600

    uwu jquery

--- app/static/scripts/edit_skills.js
+++ app/static/scripts/edit_skills.js
@@ -1,33 +1,17 @@
-function updateEntityInstances(entityType, skillId, instanceDropdownId) {
-  fetch(`/get_available_entities/${entityType}/${skillId}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
+$(document).ready(function() {
+  $("[id^=entityTypeSelect-]").change(function() {
+    let skillIdParts = this.id.split("-");
+    let skillId = skillIdParts.slice(1).join("-");
+    let instanceDropdownId = `entityInstanceSelect-${skillId}`;
+
+    $.getJSON(`/get_available_entities/${this.value}/${skillId}`, function(data) {
       let options = '<option value="">Select Instance</option>';
-      data.forEach((entity) => {
+      $.each(data, function(key, entity) {
         options += `<option value="${entity.id}">${entity.name}</option>`;
       });
-      document.getElementById(instanceDropdownId).innerHTML = options;
-    })
-    .catch((error) => {
+      $(`#${instanceDropdownId}`).html(options);
+    }).fail(function(error) {
       console.error("There was a problem with the fetch operation:", error);
-    });
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("[id^=entityTypeSelect-]").forEach((dropdown) => {
-    dropdown.addEventListener("change", function () {
-      let skillIdParts = this.id.split("-");
-      let skillId = skillIdParts.slice(1).join("-");
-      updateEntityInstances(
-        this.value,
-        skillId,
-        `entityInstanceSelect-${skillId}`
-      );
     });
   });
 });
@@ -39,5 +23,5 @@ function updateSkillLevelText(value, elementId) {
   } else if (value == 3) {
     levelText = "Advanced";
   }
-  document.getElementById(elementId).textContent = levelText;
+  $(`#${elementId}`).text(levelText);
 }
