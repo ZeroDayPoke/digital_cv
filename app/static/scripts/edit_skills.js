7d17e0338bcc86ba08a7aa416f2906d2f9ac4825
Chris Stamper <cstampok@gmail.com>
2023-11-12 17:16:41 -0600

    jquery dynamic 2nddrop display

--- app/static/scripts/edit_skills.js
+++ app/static/scripts/edit_skills.js
@@ -1,16 +1,30 @@
-$(document).ready(function() {
-  $("[id^=entityTypeSelect-]").change(function() {
+$(document).ready(function () {
+  // Handler for EntityTypeSelect dropdown change
+  $("[id^=entityTypeSelect-]").change(function () {
     let skillIdParts = this.id.split("-");
     let skillId = skillIdParts.slice(1).join("-");
+    let entityTypeSelectId = `entityTypeSelect-${skillId}`;
     let instanceDropdownId = `entityInstanceSelect-${skillId}`;
 
-    $.getJSON(`/get_available_entities/${this.value}/${skillId}`, function(data) {
-      let options = '<option value="">Select Instance</option>';
-      $.each(data, function(key, entity) {
-        options += `<option value="${entity.id}">${entity.name}</option>`;
-      });
-      $(`#${instanceDropdownId}`).html(options);
-    }).fail(function(error) {
+    // Show or hide the instance dropdown based on selection
+    var selectedValue = $(`#${entityTypeSelectId}`).val();
+    if (selectedValue) {
+      $(`#${instanceDropdownId}`).show();
+    } else {
+      $(`#${instanceDropdownId}`).hide();
+    }
+
+    // Fetch available entities and populate options
+    $.getJSON(
+      `/get_available_entities/${selectedValue}/${skillId}`,
+      function (data) {
+        let options = '<option value="">Select Instance</option>';
+        $.each(data, function (key, entity) {
+          options += `<option value="${entity.id}">${entity.name}</option>`;
+        });
+        $(`#${instanceDropdownId}`).html(options);
+      }
+    ).fail(function (error) {
       console.error("There was a problem with the fetch operation:", error);
     });
   });
