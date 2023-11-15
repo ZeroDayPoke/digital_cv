cc48ed7a05d920d533c3bff0d334c5438202f512
Chris Stamper <cstampok@gmail.com>
2023-11-15 14:05:51 -0600

    populate project images

--- app/utils/file_upload_helper.py
+++ app/utils/file_upload_helper.py
@@ -4,42 +4,34 @@ import os
 from werkzeug.utils import secure_filename
 from flask import request, current_app, flash
 
-
 def allowed_file(filename):
     """
     Check if the given filename has an allowed extension.
-
     Args:
         filename (str): The name of the file to check.
-
     Returns:
         bool: True if the file has an allowed extension, False otherwise.
     """
     allowed_extensions = current_app.config['ALLOWED_EXTENSIONS']
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
-
 def handle_file_upload(model_name, field_name='image'):
     """
     Handles file upload for a given model and field name.
-
     Args:
         model_name (str): The name of the model to upload the file for.
         field_name (str): The name of the file field in the request. Defaults to 'image'.
-
     Returns:
         str: The filename of the uploaded file if successful, None otherwise.
     """
-    if field_name not in request.files:
-        flash('Error: File not found or not allowed.', 'danger')
+    file = request.files.get(field_name)
+
+    if not file or file.filename == '':
         return None
 
-    file = request.files.get(field_name)
-    if file and allowed_file(file.filename):
+    if allowed_file(file.filename):
         filename = secure_filename(file.filename)
-
-        upload_folder = os.path.join(
-            current_app.config['UPLOAD_FOLDER'], model_name)
+        upload_folder = os.path.join(current_app.config['UPLOAD_FOLDER'], model_name)
 
         if not os.path.exists(upload_folder):
             os.makedirs(upload_folder)
@@ -48,5 +40,5 @@ def handle_file_upload(model_name, field_name='image'):
         file.save(file_path)
         return filename
 
-    flash('Error: File not found or not allowed.', 'danger')
+    flash('Error: Invalid file extension.', 'danger')
     return None
